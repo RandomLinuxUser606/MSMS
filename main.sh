09a1bfb96eb92ab86ff68ecc0dd4f089b7d7ebeb
RandomLinuxUser606 <davizinedicao@protonmail.com>
2025-07-15 00:10:16 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-# By RandomLinuxUser606, version 0.1.
+# Copyright (C) 2025 RandomLinuxUser606 <davizinedicao@protonmail.com>  
+# 
+# This program is free software: you can redistribute it and/or modify  
+# it under the terms of the GNU General Public License as published by  
+# the Free Software Foundation, either version 3 of the License, or  
+# any later version.  
+#  
+# This program is distributed in the hope that it will be useful,  
+# but WITHOUT ANY WARRANTY; without even the implied warranty of  
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the  
+# GNU General Public License for more details.  
+#  
+# You should have received a copy of the GNU General Public License  
+# along with this program. If not, see <https://www.gnu.org/licenses/>.  
 echo "MSMS - Version 0.1"
 echo "A CLI and open-source utility to simplify creating"
 echo "and managing a Minecraft server."
